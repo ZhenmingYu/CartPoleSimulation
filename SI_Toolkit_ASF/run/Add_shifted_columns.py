645508f372eb7ad0945bbe2b9d476d14131136f7
ZhenmingYu <z.yu@fz-juelich.de>
2023-07-08 09:07:09 -0600

    sync

--- SI_Toolkit_ASF/run/Add_shifted_columns.py
+++ SI_Toolkit_ASF/run/Add_shifted_columns.py
@@ -9,9 +9,9 @@ from SI_Toolkit.load_and_normalize import add_shifted_columns
 # get_files_from = 'SI_Toolkit_ASF/Experiments/DG-27s-and-1s500ms-noisy/Recordings/'+folder
 # save_files_to = 'SI_Toolkit_ASF/Experiments/DG-27s-and-1s500ms-noisy-u/Recordings/'+folder
 
-get_files_from = 'Experiment_Recordings/TestingAdaptiveNet'
-save_files_to = 'Experiment_Recordings/TestingAdaptiveNet-u'
-variables_to_shift = ['u']
+get_files_from = './SI_Toolkit_ASF/Experiments/10%_randomwalk_on_L_every_0.002s/Models/Recordings/'
+save_files_to = './SI_Toolkit_ASF/Experiments/10%_randomwalk_on_L_every_0.002s/Models/Recordings/'
+variables_to_shift = ['angle_sin', 'angle_cos', 'angleD', 'position', 'positionD']
 indices_by_which_to_shift = [-1]
 
 if __name__ == '__main__':
